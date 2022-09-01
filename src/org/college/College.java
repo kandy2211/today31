d624fb9003f9b856c6e47a0f039f59f9b67af42d
kandy2211 <kanthasamy2211@gmail.com>
2022-09-01 10:12:45 +0530

    fisrt

---
+++ src/org/college/College.java
@@ -0,0 +1,20 @@
+package org.college;
+
+public class College     {
+public void collegeName() {
+System.out.println("jpr");
+
+}
+public void collegeCode() {
+System.out.println("651584");
+}
+protected void collegeRank() {
+System.out.println("1");
+}
+public static void main(String[] args) {
+	
+
+	
+	
+}
+}
---
+++ src/org/college/Hostel.java
@@ -0,0 +1,14 @@
+package org.college;
+
+public class Hostel extends Dept {
+public void hostelName() {
+System.out.println("boysb1b");
+}
+public static void main(String[] args) {
+	Dept b = new Dept();
+	b.deptName();
+	
+}
+
+
+}
