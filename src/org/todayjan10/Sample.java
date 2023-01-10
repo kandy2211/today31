06734d8e10776b37d3b85bb6d3528136130e351e
win7 <win7@win7-PC>
2023-01-10 15:29:00 +0530

    kona branch

---
+++ src/org/todayjan10/Sample.java
@@ -0,0 +1,17 @@
+package org.todayjan10;
+
+import org.college.College;
+
+public class Sample extends College {
+
+	public static void main(String[] args) {
+
+		Sample a = new Sample();
+
+		a.collegeCode();
+		a.collegeName();
+		a.collegeRank();
+
+	}
+
+}
